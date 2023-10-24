9006d79073ef203cccb114c7339e57d26702631d
lucasmfs23 <142855185+lucasmfs23@users.noreply.github.com>
2023-10-24 19:31:51 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,16 @@
+$(".drop")
+  .mouseover(function() {
+  $(".dropdown").show(300);
+});
+$(".drop")
+  .mouseleave(function() {
+  $(".dropdown").hide(300);     
+});
+$(".drop1")
+  .mouseover(function() {
+  $(".dropdown1").show(300);
+});
+$(".drop1")
+  .mouseleave(function() {
+  $(".dropdown1").hide(300);     
+});
